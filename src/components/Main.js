bad8e8ee4a23270da51935361d683d68a05d49d9
Waqar Habib <waqarhabib94@aggienetwork.com>
2019-08-03 23:51:28 -0500

    created an add patient form and added routes for it

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Patients from './Patients';
 import Home from './Home';
 import PatientDetails from './PatientDetails';
+import AddPatient from './AddPatients';
 
 const Main = () => (
     <main>
@@ -10,6 +11,7 @@ const Main = () => (
             <Route exact path='/' component={Home} />
             <Route exact path='/patients' component={Patients} />
             {/* Any other pages you create, add a new Route tag and link that component by importing it up top and then adding it here */}
+            <Route exact path='/patients/add' component={AddPatient} />
             <Route exact path='/patients/:id' component={PatientDetails} />
         </Switch>
     </main>
