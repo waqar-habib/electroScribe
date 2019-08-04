7e78d8e292c091ce938266344ecd9d713bdfcff9
Waqar Habib <waqarhabib94@aggienetwork.com>
2019-08-04 12:51:56 -0500

    created onSubmit function that POSTs a new entry to the API

--- src/components/AddPatients.js
+++ src/components/AddPatients.js
@@ -3,8 +3,35 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 class AddPatient extends Component {
-    onSubmit(){
-        return 1;
+    // Creating an addPatient method to take in the newPatient object w/ the form data
+    addPatient(newPatient){
+        //console.log(newPatient)
+        // Making a POST request thru axios
+        axios.request({
+            method: 'POST',
+            url: 'http://[::1]:3000/patients/',
+            data: newPatient
+        }).then(response => {
+            // Redirecting to patients page
+            this.props.history.push('/patients');
+        }).catch(err => console.log(err));
+    };
+
+    // Creating an onSubmit method that takes in "e" or event which is the input from the form and stores it in newPatient object. 
+    onSubmit(e){
+        //console.log(this.refs.name.value);
+        // Making sure to create an array of object [{}] since this is the format the API accepts POST requests
+        const newPatient = [{
+            pt_uuid: this.refs.pt_uuid.value,
+            pt_name: this.refs.pt_name.value,
+            pt_email: this.refs.pt_email.value,
+            pt_cell:  this.refs.pt_cell.value,
+            pt_gender: this.refs.pt_gender.value,
+            pt_dob: this.refs.pt_dob.value 
+        }]
+        // Adds the newPatient object using the addPatient method created above. 
+        this.addPatient(newPatient);
+        e.preventDefault();
     }
 
     render(){
@@ -17,41 +44,42 @@ class AddPatient extends Component {
                     <div className="row">
                         <div className="input-field col s12">
                             <input name="pt_uuid" ref="pt_uuid" placeholder="1688546c-e060-4bb4-a5ba-bb1af8846f30" id="pt_uuid" type="text" className="validate" />
-                            <label class="active" htmlFor="pt_uuid">Patient ID</label>
-                            <span class="helper-text">Visit https://www.uuidgenerator.net/ to generate a unique patient ID number</span>
+                            <label className="active" htmlFor="pt_uuid">Patient ID</label>
+                            <span className="helper-text">Visit https://www.uuidgenerator.net/ to generate a unique patient ID number</span>
                         </div>
                     </div>                    
                     <div className="row">
                         <div className="input-field col s6">
-                            <input name="name" ref="name" placeholder="John Doe" id="name" type="text" className="validate" />
-                            <label class="active" htmlFor="name">Name</label>
+                            <input name="pt_name" ref="pt_name" placeholder="John Doe" id="pt_name" type="text" className="validate" />
+                            <label className="active" htmlFor="pt_name">Name</label>
                         </div>
                         <div className="input-field col s6">
-                            <input name="email" ref="email" placeholder="john.doe@test.com" id="email" type="text" className="validate" />
-                            <label class="active"  htmlFor="email">Email</label>
+                            <input name="pt_email" ref="pt_email" placeholder="john.doe@test.com" id="pt_email" type="text" className="validate" />
+                            <label className="active"  htmlFor="pt_email">Email</label>
                         </div>                        
                     </div>
 
                     <div className="row">
                         <div className="input-field col s6">
-                            <input name="cell" ref="cell" placeholder="346-252-3365" id="cell" type="text" className="validate" />
-                            <label class="active"  htmlFor="cell">Cell</label>
+                            <input name="pt_cell" ref="pt_cell" placeholder="346-252-3365" id="pt_cell" type="text" className="validate" />
+                            <label className="active"  htmlFor="pt_cell">Cell</label>
+                            <span className="helper-text">Cell number must be in XXX-XXX-XXXX format</span>
                         </div>
                         <div className="input-field col s6">
-                            <input name="gender" ref="gender" placeholder="Male" id="gender" type="text" className="validate" />
-                            <label class="active"  htmlFor="gender">Gender</label>
+                            <input name="pt_gender" ref="pt_gender" placeholder="Male" id="pt_gender" type="text" className="validate" />
+                            <label className="active"  htmlFor="pt_gender">Gender</label>
                         </div>
                     </div>
 
                     <div className="row">
                         <div className="input-field col s6">
-                            <input name="dob" ref="dob" placeholder="1994-01-01" id="dob" type="text" className="validate" />
-                            <label class="active"  htmlFor="dob">Date of Birth</label>
-                            <span class="helper-text">Date must be in YYYY-MM-DD format</span>
+                            <input name="pt_dob" ref="pt_dob" placeholder="1994-01-01" id="pt_dob" type="text" className="validate" />
+                            <label className="active"  htmlFor="pt_dob">Date of Birth</label>
+                            <span className="helper-text">Date must be in YYYY-MM-DD format</span>
                         </div>
                     </div>
                     <div className="row">
-                        <button class="btn waves-effect waves-light" type="submit" value="Save" name="action">Add Patient</button> 
+                        <button className="btn waves-effect waves-light" type="submit" value="Save" name="action">Add Patient</button> 
                     </div>                                                                                                    
                 </form>
             </div>
