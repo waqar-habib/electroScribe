fc43c90e4f340680303464747b7b4c9e9c7c2f06
Andrew Bayles <baylesweb@gmail.com>
2019-08-07 18:13:36 -0500

    Add patient list to medical records page

--- src/components/pages/Medical-records.js
+++ src/components/pages/Medical-records.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 class MedRecords extends Component {
     constructor() {
         super();
+        // Create a state object
+        this.state = {
+            // Set patients to an empty array. You will have to use the same method when you are pulling in drugs, conditions and providers as well.
+            patients: []
+        }
     }
 
     // Create a componentWillMount lifecycle method
     componentWillMount() {
         // This will go to the backend and retrieve the info from LB API
+        this.getPatients();
+    }
+
+    // Create a new method getPatients to make an axios request
+    getPatients() {
+        axios.get('http://[::1]:3000/Patients')
+            .then(response => {
+                // Setting the patients state object to the response
+                this.setState({patients: response.data}, () => {
+                    console.log(this.state);
+                })
+            })
+            .catch(err => console.log(err))
     }
 
     // Render is a lifecyle method
@@ -16,9 +35,35 @@ class MedRecords extends Component {
         return (
             <div>
                 <h1>Medical Records</h1>
-                <ul className="collection">
-                    
-                </ul>
+                <table className="highlight">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Date of Birth</th>
+                            <th>Cell Number</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.patients.map((patient, i) => {
+                            return (
+                                <tr key={i}>
+                                    <td>
+                                        <Link to={`/patients/${patient.pt_uuid}`}>
+                                        {patient.pt_name}</Link>
+                                    </td>
+                                    <td>{patient.pt_dob}</td>
+                                    <td>{patient.pt_cell}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+
+                <div className="fixed-action-button">
+                    <Link to="/patients/add" className="btn-floating btn-large red">
+                        <i className="fa fa-plus"></i>
+                    </Link>
+                </div>
             </div>
 
         )
