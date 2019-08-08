0cc1811224fc574a828cc2825db42c08d5bb67ae
Fatih Ersoy <f.ersoytx@gmail.com>
2019-08-07 23:23:28 -0500

    redesigned folders

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import Main from './components/Main';
-import Navbar from './components/Navbar';
+import Main from './components/Pages/Main';
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => (
   <div>
