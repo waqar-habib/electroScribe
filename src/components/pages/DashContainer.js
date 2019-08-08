a5f1558b8f446e8b1b198cf0617aaf34d5ebd6fb
Andrew Bayles <baylesweb@gmail.com>
2019-08-08 11:02:47 -0500

    Initial dash container components

--- src/components/pages/DashContainer.js
+++ src/components/pages/DashContainer.js
@@ -1,29 +1,23 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import DashContainer from './DashContainer';
 
-class Dashboard extends Component {
+class DashContainer extends Component {
     constructor() {
         super();
     }
 
     // Create a componentWillMount lifecycle method
     componentWillMount() {
-        // This will go to the backend and retrieve the info from LB API
     }
 
     // Render is a lifecyle method
     render() {
         return (
             <div>
-                <h1>Dashboard</h1>
-                <ul className="collection">
-                    <DashContainer />
-                </ul>
+                <p>Dash container.</p>
             </div>
-
         )
     }
 }
 
-export default Dashboard;
+export default DashContainer;
