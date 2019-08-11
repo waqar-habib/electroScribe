fbbbdb2cb96546c6510f6d6639bea17dfecac138
Waqar Habib <waqarhabib94@aggienetwork.com>
2019-08-11 11:38:34 -0500

    compiled api and app together to run on same port

---
+++ build/precache-manifest.4b36f622347e6c32411232c146a35532.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7787aa0c88659b1d2de52aebaea30173",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae52fa2054f262573d34",
+    "url": "/static/css/main.d352828a.chunk.css"
+  },
+  {
+    "revision": "ea2684b787dea9cfb849",
+    "url": "/static/js/2.70a081a8.chunk.js"
+  },
+  {
+    "revision": "ae52fa2054f262573d34",
+    "url": "/static/js/main.27bdb170.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
