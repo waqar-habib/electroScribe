76ce24e2a645a83f904474fb01c0df684383f1c7
Waqar Habib <waqarhabib94@aggienetwork.com>
2019-08-12 22:59:33 -0500

    changed pt_dob from DATETIME to STRING

--- seeds/patients_seeds.sql
+++ seeds/patients_seeds.sql
@@ -1,12 +1,12 @@
 USE electroscribe_db;
 
-INSERT INTO patients VALUES ("1","Andy Dwyer","Andy.Dwyer@gmail.com","355-249-7604","Male","1978-12-10");
-INSERT INTO patients VALUES ("2","Ben Wyatt","Ben.Wyatt@gmail.com","427-712-1663","Male","1980-12-19");
-INSERT INTO patients VALUES ("3","Chris Traeger","Chris.Traeger@gmail.com","371-713-5650","Male","1971-12-15");
-INSERT INTO patients VALUES ("4","Ann Perkins","Ann.Perkins@gmail.com","882-353-3706","Female","1980-11-02");
-INSERT INTO patients VALUES ("5","Jerry Gergich","Jerry.Gergich@gmail.com","605-488-0353","Male","1959-10-03");
-INSERT INTO patients VALUES ("6","Tom Haverford","Tom.Haverford@gmail.com","735-773-5589","Male","1984-01-10");
-INSERT INTO patients VALUES ("7","Donna Meagle","Donna.Meagle@gmail.com","930-309-4112","Female","1978-10-12");
-INSERT INTO patients VALUES ("8","Ron Swanson","Ron.Swanson@gmail.com","714-429-7405","Male","1965-07-03");
-INSERT INTO patients VALUES ("9","Leslie Knope","Leslie.Knope@gmail.com","717-871-5489","Female","1980-05-03");
-INSERT INTO patients VALUES ("10","April Ludgate","April.Ludgate@gmail.com","782-270-1507","Female","1983-06-04");
+INSERT INTO patients VALUES ("1","Andy Dwyer","Andy.Dwyer@gmail.com","355-249-7604","Male","12-10-1978");
+INSERT INTO patients VALUES ("2","Ben Wyatt","Ben.Wyatt@gmail.com","427-712-1663","Male","12-19-1980");
+INSERT INTO patients VALUES ("3","Chris Traeger","Chris.Traeger@gmail.com","371-713-5650","Male","12-15-1971");
+INSERT INTO patients VALUES ("4","Ann Perkins","Ann.Perkins@gmail.com","882-353-3706","Female","11-02-1980");
+INSERT INTO patients VALUES ("5","Jerry Gergich","Jerry.Gergich@gmail.com","605-488-0353","Male","10-03-1959");
+INSERT INTO patients VALUES ("6","Tom Haverford","Tom.Haverford@gmail.com","735-773-5589","Male","01-10-1984");
+INSERT INTO patients VALUES ("7","Donna Meagle","Donna.Meagle@gmail.com","930-309-4112","Female","10-12-1978");
+INSERT INTO patients VALUES ("8","Ron Swanson","Ron.Swanson@gmail.com","714-429-7405","Male","07-03-1965");
+INSERT INTO patients VALUES ("9","Leslie Knope","Leslie.Knope@gmail.com","717-871-5489","Female","05-03-1980");
+INSERT INTO patients VALUES ("10","April Ludgate","April.Ludgate@gmail.com","782-270-1507","Female","06-04-1983");
